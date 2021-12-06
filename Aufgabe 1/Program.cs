738fded0b39dd20d74a195e1694c573a29124492
Enes Uensal <enes.uensal147123@gmail.com>
2021-12-06 09:42:05 +0100

    Upload Funktionierender Rechner

---
+++ Aufgabe 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Aufgabe_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double Provision;
+            double GesamtProvision = 0;
+            string weiter = "Y";
+            do {
+
+                Console.WriteLine("\nGeben Sie den Gaeschaftsall ein: ");
+                double Geschaeftsfall = Convert.ToDouble(Console.ReadLine());
+                if (Geschaeftsfall > 10000)
+                {
+                    Provision = Geschaeftsfall / 100 * 6;
+                    Console.WriteLine("Ihre Provision: " + Provision);
+                    GesamtProvision = GesamtProvision + Provision;
+                    Console.WriteLine("\nNeue Gesamtprovision: " + GesamtProvision + "\n");
+                }
+                else
+                {
+                    Provision = Geschaeftsfall / 100 * 3;
+                    Console.WriteLine("Ihre Provision: " + Provision);
+                    GesamtProvision = GesamtProvision + Provision;
+                    Console.WriteLine("\nNeue Gesamtprovision: " + GesamtProvision + "\n");
+                }
+
+                Console.WriteLine("Möchten Sie weitere Geschäftsfälle eingeben");
+                Console.WriteLine("y für Weitermachen");
+                weiter = Console.ReadLine();
+
+            }while (weiter.ToLower() == "y");
+
+            Console.WriteLine("\nIhre Gesamtprovision: \t" + GesamtProvision);
+        }
+    }
+}
