58971a947159f2be475fdbccb07c9613300c8127
Enes Uensal <enes.uensal147123@gmail.com>
2021-12-17 14:11:22 +0100

    Programm gekuerzt

--- Aufgabe 1/Program.cs
+++ Aufgabe 1/Program.cs
@@ -17,24 +17,21 @@ namespace Aufgabe_1
                 if (Geschaeftsfall > 10000)
                 {
                     Provision = Geschaeftsfall / 100 * 6;
-                    Console.WriteLine("Ihre Provision: " + Provision);
-                    GesamtProvision = Math.Round(GesamtProvision + Provision,2);
-                    Console.WriteLine("\nNeue Gesamtprovision: " + GesamtProvision + "\n");
                 }
                 else
                 {
                     Provision = Geschaeftsfall / 100 * 3;
-                    Console.WriteLine("Ihre Provision: " + Provision);
-                    GesamtProvision = Math.Round(GesamtProvision + Provision, 2);
-                    Console.WriteLine("\nNeue Gesamtprovision: " + GesamtProvision + "\n");
                 }
-
+                GesamtProvision = Math.Round(GesamtProvision + Provision, 2);
+                
+                Console.WriteLine("Ihre Provision: " + Math.Round(Provision,2) + " EURO");
                 Console.WriteLine("Möchten Sie weitere Geschäftsfälle eingeben [Y/N]");
+                
                 weiter = Console.ReadKey();
-
+                Console.Clear();
             }while (weiter.Key == ConsoleKey.Y);
 
-            Console.WriteLine("\nIhre Gesamtprovision: \t" + Math.Round(GesamtProvision,2));
+            Console.WriteLine("\nIhre Gesamtprovision: \t" + Math.Round(GesamtProvision,2) + " EURO");
         }
     }
 }
